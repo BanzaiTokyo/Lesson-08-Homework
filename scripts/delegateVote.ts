2636dfbb3ff0ae2797fce1d0c16c8f198eb71688
Mauromonso <57332446+Mauromonso@users.noreply.github.com>
2024-03-24 17:52:46 +0100

    Add files via upload
    //Work in progress, still trowing error

---
+++ scripts/delegateVote.ts
@@ -0,0 +1,60 @@
+import { sepolia } from "viem/chains";
+import * as dotenv from "dotenv";
+dotenv.config();
+import { createPublicClient, http, createWalletClient } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { abi } from "../artifacts/contracts/Ballot.sol/Ballot.json";
+
+const providerApiKey = process.env.ALCHEMY_API_KEY || "";
+const deployerPrivateKey = process.env.PRIVATE_KEY || "";
+
+async function main() {
+  const parameters = process.argv.slice(2);
+  if (!parameters || parameters.length < 2)
+    throw new Error("Parameters not provided");
+  const contractAddress = parameters[0] as `0x${string}`;
+  if (!contractAddress) throw new Error("Contract address not provided");
+  if (!/^0x[a-fA-F0-9]{40}$/.test(contractAddress))
+    throw new Error("Invalid contract address");
+  const delegateAddress = parameters[1];
+  
+  if (!delegateAddress) throw new Error("Delegate address not provided");
+  const publicClient = createPublicClient({
+    chain: sepolia,
+    transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
+  });
+  const account = privateKeyToAccount(`0x${deployerPrivateKey}`);
+ const delegator = createWalletClient({
+    account,
+    chain: sepolia,
+    transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
+  });
+ 
+  console.log("Do you want to delegate the vote to", delegateAddress, "?");
+  console.log("Confirm? (Y/n)");
+
+  const stdin = process.openStdin();
+  stdin.addListener("data", async function (d) {
+    if (d.toString().trim().toLowerCase() != "n") {
+      const hash = await delegator.writeContract({
+        address: contractAddress,
+        abi,
+        functionName: "delegate",
+        args: [delegateAddress],
+      });
+      console.log("Transaction hash:", hash);
+      console.log("Waiting for confirmations...");
+      const receipt = await publicClient.waitForTransactionReceipt({ hash });
+      console.log(delegateAddress, "is the new delegated address for ",account);
+      // console.log(receipt);, can be used to get more infos about the Tx
+    } else {
+      console.log("Operation cancelled");
+    }
+    process.exit();
+  });
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
